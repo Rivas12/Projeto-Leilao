97297e410ae2e133344b836967afbfbbf02dbdb0
Rivaldo Silveira <54002401+Rivas12@users.noreply.github.com>
2022-05-30 23:17:31 -0300

    Add files via upload

---
+++ index/lances.js
@@ -0,0 +1,41 @@
+var dataa = new Date();
+var dia = String(dataa.getDate()).padStart(2, '0');
+var mes = String(dataa.getMonth() + 1).padStart(2, '0');
+var ano = dataa.getFullYear();
+
+dataAtual = dia + '/' + mes + '/' + ano;
+
+
+function enviaMsg(id) {
+    const xhttp = new XMLHttpRequest();
+  
+    xhttp.onreadystatechange = function() {
+      console.log("readyState:" + this.readyState);
+      console.log("status:" + this.status);
+  
+      if (this.readyState == 4 && this.status == 200) {
+        imprimeResposta(this);
+      }
+    };
+  
+    xhttp.onload = function() {
+      document.getElementById("resposta").innerHTML = this.responseText;
+    };
+    var data = {
+    "id_usuario": 1,
+    "id_produto": id,
+    "valor":document.getElementById("produto" + id).value,
+    "data": dataAtual,
+    };
+    var jsondata = JSON.stringify(data);
+    var url = "https://9suxx80va7.execute-api.us-east-1.amazonaws.com/teste";
+    xhttp.open("POST", url);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(jsondata);
+  }
+  
+  function imprimeResposta(xml) {
+    var xmlDoc = xml.responseXML;
+    console.log(xmlDoc)
+    document.getElementById("resposta").innerHTML = xmlDoc;
+  }
